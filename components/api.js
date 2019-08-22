100740c187372619c6c741aa5592b5f0b4584d91
TBthegr8 <tbthegr81@gmail.com>
2019-08-22 07:08:57 +0200

    Upgrades to server-join notifications in API, and added functionality to make leaflet maps containing a route from GPX file

--- components/api.js
+++ components/api.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const formidable = require('formidable');
+const fs = require('fs');
+const mv = require('mv');
+const parser = require('xml2json');
+const {distanceBetweenPoints, speedBetweenPoints, leaflet} = require("./mapLib");
+import commandController from './commandController';
+import serviceController from './serviceController';
 
 class Api {
-  constructor(cc, sc, set) {
-    this.cc = cc;
-    this.sc = sc;
-    this.set = set;
-    console.log(set.settings.api_port);
+  constructor() {
     this.versionnumber = 1;
+    this.port = null;
+  }
 
-    if(set.settings.api_port != null) {
+  setPort(port) {
+    this.port = port;
+  }
+
+  start() {
+    if(this.port != null) {
       const app = express();
       var router = express.Router();
 
@@ -30,30 +40,98 @@ class Api {
       router.post('/api/v'+this.versionnumber+'/gameserver/playerJoined', function(req, res) {
         console.log("Player Joined");
         console.log(req.body);
-        //console.log(this.sc);
-        let line = req.body.username+" joined Snekabel "+req.body.game+" server";
+        //console.log(serviceController);
+        let line = req.body.username+" joined "+req.body.game+" server";
         console.log(line);
-        //this.sc.services["mumble"].writeLine("Snekabel", line);
-        this.sc.services["discord"].writeLine("skitsnack", line);
+        //serviceController.services["mumble"].writeLine("Snekabel", line);
+        serviceController.services["discord"].writeLine("bs", line);
         res.send(JSON.stringify({status:"ok", playerName: req.body.username}));
       }.bind(this))
       router.post('/api/v'+this.versionnumber+'/gameserver/playerDisconnected', function(req, res) {
         console.log("Player Disconnected");
         console.log(req.body);
-        //console.log(this.sc);
-        let line = req.body.username+" left Snekabel "+req.body.game+" server";
+        //console.log(serviceController);
+        let line = req.body.username+" left "+req.body.game+" server";
         console.log(line);
-        //this.sc.services["mumble"].writeLine("Snekabel", line);
-        this.sc.services["discord"].writeLine("skitsnack", line);
+        //serviceController.services["mumble"].writeLine("Snekabel", line);
+        serviceController.services["discord"].writeLine("bs", line);
         res.send(JSON.stringify({status:"ok", playerName: req.body.username}));
       }.bind(this))
 
+      router.get('/api/v'+this.versionnumber+'/map/distance/:lat1/:lon1/:lat2/:lon2', function(req, res) {
+        if (req.params.lat1 && req.params.lon1 && req.params.lat2 && req.params.lon2) {
+            let pos1 = {lat: req.params.lat1, lon: req.params.lon1};
+            let pos2 = {lat: req.params.lat2, lon: req.params.lon2};
+  		      res.send(JSON.stringify(distanceBetweenPoints(pos1, pos2)+"m"));
+	       }
+         else {
+           res.send("Nothing");
+         }
+      })
+      router.get('/api/v'+this.versionnumber+'/map/distance/:lat1/:lon1/:lat2/:lon2/:time1/:time2', function(req, res) {
+        if (req.params.lat1 && req.params.lon1 && req.params.time1 && req.params.lat2 && req.params.lon2 && req.params.time2) {
+            let pos1 = {lat: req.params.lat1, lon: req.params.lon1};
+            let pos2 = {lat: req.params.lat2, lon: req.params.lon2};
+            res.send(JSON.stringify(speedBetweenPoints(pos1, req.params.time1, pos2, req.params.time2)+"km/h"));
+         }
+         else {
+           res.send("Nothing");
+         }
+      })
+      router.get('/html/map/', function(req, res) {
+        var token = "pk.eyJ1IjoidGJ0aGVncjgxIiwiYSI6ImNqdG80YTFnNzA1cHg0OXF0ZW81eDBtemkifQ.zdnYP1ufAPburgumLeECFw";
+        leaflet(res, token);
+      });
+      router.get('/html/map/:gpxfile', function(req, res) {
+        var token = "pk.eyJ1IjoidGJ0aGVncjgxIiwiYSI6ImNqdG80YTFnNzA1cHg0OXF0ZW81eDBtemkifQ.zdnYP1ufAPburgumLeECFw";
+        leaflet(res, token, req.params.gpxfile);
+      });
+
+      router.post('/api/gpx', function(req, res) {
+        // Handle uploading of GPX file
+        console.log("Uploading GPX file!");
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+          for (var file in files) {
+            if (files.hasOwnProperty(file)) {
+              //console.log(files[file]);
+              var oldpath = files[file].path;
+              var newpath = '/mnt/raid/node/skbot/gpx/' + files[file].name;
+              mv(oldpath, newpath, function(err) {
+                if (err) {
+                  error.log(err);
+                  res.write("Error");
+                }
+                else {
+                    res.write('File uploaded and moved!');
+                }
+                res.end();
+              });
+            }
+          }
+        });
+      });
+
+      router.get('/api/gpx/:gpxfile', function(req, res) {
+        fs.readFile('gpx/'+req.params.gpxfile, 'utf8', function(err, contents) {
+          if (err) {
+            error.log(err);
+            res.write("Error");
+          }
+          else {
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.write(parser.toJson(contents));
+          }
+          res.end();
+        });
+      });
+
       app.use(router);
 
-      app.listen(set.settings.api_port, () => console.log('SKBot listening on port '+set.settings.api_port+'!'));
+      app.listen(this.port, () => console.log('SKBot listening on port '+this.port+'!'));
     }
-
   }
 }
+var api = new Api();
 
-export default Api;
+export default api;
